ec0e0083b53aa2dc4ef33186730fd0f8c81c82c0
Ali <alimohalghurbani@gmail.com>
2023-02-08 15:22:00 +0100

    opdracht12

--- site/index.php
+++ site/index.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mijn eerste pagina</title>
+</head>
+<body>
 
- echo "im a big fan of the simpsons";
+    <?php
+
+    echo " im a big fan of the simpsons"
+    
+
+    ?>
+    
+</body>
+</html>
